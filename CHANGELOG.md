199e5396020ebf17ca315ab80c077bbd7693dd91
Brian Faust <hello@brianfaust.de>
2017-06-05 13:20:04 +0300

    Some dependency changes

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog
-
-All Notable changes to `faustbrian/laravel-utilities` will be documented in this file.
-
-## 1.0.0 (11/04/2017)
-- First release
--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -1,14 +1,9 @@
 <?php
 
-
-
-declare(strict_types=1);
-
 namespace BrianFaust\Tests\Utilities;
 
 use PHPUnit_Framework_TestCase as AbstractPackageTestCase;
 
 abstract class AbstractTestCase extends AbstractPackageTestCase
 {
-    //
 }
